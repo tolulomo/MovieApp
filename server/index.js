a27fe633f22cb686788cd2705652a4ad9cd76f26
Tolulomo Fateye <tolulomofateye@gmail.com>
2020-01-21 04:45:16 -0500

    Updated for heroku deployment

--- server/index.js
+++ server/index.js
@@ -29,12 +29,12 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our App.js file, or main.css file!
-    app.use(express.static('build'));
+    app.use(express.static('docs'));
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'docs', 'index.html'));
     });
 }
 
