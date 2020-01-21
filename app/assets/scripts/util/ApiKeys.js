3449bfd10a34096c46df85354556a028d44120eb
Tolulomo Fateye <tolulomofateye@gmail.com>
2020-01-21 05:50:55 -0500

    update for heroku deoployment

--- app/assets/scripts/util/ApiKeys.js
+++ app/assets/scripts/util/ApiKeys.js
@@ -3,7 +3,7 @@
 if(process.env.NODE_ENV === 'production'){
     // run production keys
     module.exports = {
-        apiKey: process.env.API_KEY
+        apiKey: process.env.API_KEY || '9084eae9f770e006ebcba95dbd474e28'
     }
 } else {
     const key = require('../../../../apiKey');
